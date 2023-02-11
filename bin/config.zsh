a4061cd77a822767130b26fa5e72be57b47828b3
unforswearing <al.charity@gmail.com>
2023-02-11 11:05:20 -0500

    hardlink the correct file

--- bin/config.zsh
+++ bin/config.zsh
@@ -89,4 +89,4 @@
 # use hardlinks to keep stuff in the zsh-config dir instead of home dir
 ln -sF ~/zsh-config/.zshenv ~/.zshenv
 ln -sF ~/zsh-config/.zshrc ~/.zshrc 
-ln -sF ~/zsh-config/hosts.py ~/hosts.py
+ln -sF ~/zsh-config/usr/hosts.py ~/hosts.py
