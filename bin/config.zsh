ffad94a612f3c6a4be836f2cf2c655ded4a01702
unforswearing <al.charity@gmail.com>
2023-04-15 20:52:31 +0000

    Apr 15, 2023, 4:52 PM

--- bin/config.zsh
+++ bin/config.zsh
@@ -22,7 +22,7 @@
 	source "$ZSH_PLUGIN_DIR/hlissner/zsh-autopair/autopair.zsh"
 	source "$ZSH_PLUGIN_DIR/fzf-tab/fzf-tab.plugin.zsh"
 	source "$ZSH_PLUGIN_DIR/fzf-zsh/fzf-zsh-plugin.plugin.zsh"
-	source "$ZSH_PLUGIN_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >|/dev/null 2>&1
+	source "$ZSH_PLUGIN_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 	source "$ZSH_PLUGIN_DIR/zsh-autosuggestions/zsh-autosuggestions.zsh"
 	source "$ZSH_PLUGIN_DIR/zsh-history-substring-search/zsh-history-substring-search.zsh"
 }
