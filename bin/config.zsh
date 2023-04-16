17133f682e86f81cd7aa83b27db89bb79d7da7ac
unforswearing <al.charity@gmail.com>
2023-04-16 02:58:06 +0000

    Apr 15, 2023, 10:58 PM

--- bin/config.zsh
+++ bin/config.zsh
@@ -17,6 +17,17 @@
 	zstyle ':completion:*:matches' group 'yes'
 	zstyle ':completion:*:options' description 'yes'
 	zstyle ':completion:*' verbose true
+
+  # disable sort when completing `git checkout`
+zstyle ':completion:*:git-checkout:*' sort false
+# set descriptions format to enable group support
+zstyle ':completion:*:descriptions' format '[%d]'
+# set list-colors to enable filename colorizing
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+# preview directory's content with exa when completing cd
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath'
+# switch group using `,` and `.`
+zstyle ':fzf-tab:*' switch-group ',' '.'
 }
 {
 	source "$ZSH_PLUGIN_DIR/hlissner/zsh-autopair/autopair.zsh"
