6a015dfd56f11def741124676256348e86311509
unforswearing <al.charity@gmail.com>
2023-04-16 19:15:40 +0000

    Apr 16, 2023, 3:15 PM

--- bin/export.zsh
+++ bin/export.zsh
@@ -32,5 +32,5 @@ export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=/usr/local/share/zsh-syntax-highlighting/h
 export ZSH_HIGHLIGHT_HIGHLIGHTERS=(main cursor root line)
 
 typeset -A ZSH_HIGHLIGHT_PATTERNS
-ZSH_HIGHLIGHT_PATTERNS+=('rm -rf *' 'fg=white,bold,bg=red')
+ZSH_HIGHLIGHT_PATTERNS+=('rm -rf' 'fg=white,bold,bg=red')
 
