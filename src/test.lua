e871092677c4a665df4fe8e2c433b2d99e638507
unforswearing <al.charity@gmail.com>
2023-05-07 23:29:30 -0400

    setup tl scripts, rm 'run' alias, add luarocks path

---
+++ src/test.lua
@@ -0,0 +1,9 @@
+tl = require("tl")
+tl.loader()
+
+-- test colors.tl
+local colors = require("colors")
+print(colors.green("green"))
+print(colors.yellow("yellow"))
+print(colors.red("red"))
+print(colors.blue("blue"))
