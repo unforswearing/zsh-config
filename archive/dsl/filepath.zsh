cc165812e662abee9dafff64f5e5d801dc0394e8
Alvin <al.charity@gmail.com>
2023-05-08 15:20:22 -0400

    stop ignoring archive

---
+++ archive/dsl/filepath.zsh
@@ -0,0 +1,34 @@
+use::string >|/dev/null 2>&1
+## ---------------------------------------------
+# DSL FS
+alias ll='exa $EXA_DEFAULT_OPTIONS'
+alias ll.bw='exa $EXA_DEFAULT_OPTIONS --color=never'
+alias ll.r='exa $EXA_DEFAULT_OPTIONS -R'
+# file.backup filename.txt => filename.txt.bak
+# file.restore filename.txt => overwrites filename.txt
+function {newfile,fs.file.new}() { touch "$@"; }
+function {readfile,fs.file.read}() { print "$(<"$1")"; }
+function {copy,fs.file.copy}() { file.read "$1" | pbcopy; }
+function {bkp,fs.file.backup}() { cp "${1}"{,.bak}; }
+function {rst,fs.file.restore}() { cp "${1}"{.bak,} && rm "${1}.bak"; }
+function {fexists,fs.file.exists}() { [[ -s "${1}" ]]; }
+function {fempty,fs.file.isempty}() { [[ -a "${1}" ]] && [[ ! -s "${1}" ]]; }
+# directory actions
+function {newdir,fs.dir.new}() { mkdir "${1}"; }
+function {readdir,fs.dir.read}() { ls "${1}"; }
+function {dbkp,fs.dir.backup}() { cp -r "${1}" "${1}.bak"; }
+function {drst,fs.dir.restore}() { cp -r "${1}.bak" "${1}" && rm -rf "${1}.bak"; }
+function {pdir,fs.dir.parent}() { dirname "${1:-(pwd)}"; }
+function {dexists,fs.dir.exists}() { [[ -d "${1}" ]]; } 
+function {dempty,fs.dir.isempty}() { 
+  local count=$(ls -la "${1}" | wc -l | trim.left) 
+  [[ $count -eq 0 ]];  
+}
+# fs prefix works for files and dirs
+# filepath.abs "../../file.txt"
+fs.path() { print "$(pwd)/${1}"; }
+fs.abs() { print "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"; }
+fs.newer() { [[ "${1}" -nt "${2}" ]]; }
+fs.older() { [[ "${1}" -ot "${2}" ]]; }
+## ---------------------------------------------
+green "dsl/filepath loaded"
---
+++ archive/dsl/mathnum.zsh
@@ -0,0 +1,103 @@
+# DSL MATHNUM
+# math -------------------------------------------
+math.add() { 
+  local left="${1}"; 
+  local right="${2:-$(cat -)}"; 
+  print "$((left + right))"; 
+}
+math.sub() { 
+  local left="${1}"; 
+  local right="${2:-$(cat -)}"; 
+  print "$((left - right))"; 
+}
+math.mul() { 
+  local left="${1}"; 
+  local right="${2:-$(cat -)}"; 
+  print "$((left * right))"; 
+}
+math.div() { 
+  local left="${1}"; 
+  local right="${2:-$(cat -)}"; 
+  print "$((left / right))"; 
+}
+math.pow() { 
+  local left="${1}"; 
+  local right="${2:-$(cat -)}"; 
+  print "$((left ** right))"; 
+}
+math.mod() { 
+  local left="${1}"; 
+  local right="${2:-$(cat -)}"; 
+  print "$((left % right))"; 
+}
+math.eq() { 
+  local left="${1}"; 
+  local right="${2:-$(cat -)}";  
+  return "$((left == right))"; 
+}
+math.ne() { 
+  local left="${1}"; 
+  local right="${2:-$(cat -)}";  
+  return "$((left != right))"; 
+}
+math.gt() { 
+  local left="${1}"; 
+  local right="${2:-$(cat -)}";  
+  return "$((left > right))"; 
+}
+math.lt() { 
+  local left="${1}"; 
+  local right="${2:-$(cat -)}";  
+  return "$((left < right))"; 
+}
+math.ge() { 
+  local left="${1}"; 
+  local right="${2:-$(cat -)}";  
+  return "$((left >= right))"; 
+}
+math.le() { 
+  local left="${1}"; 
+  local right="${2:-$(cat -)}";  
+  return "$((left <= right))"; 
+}
+incr ++() { local opt="${1:-$(cat -)}"; print $((++opt)); }
+decr --() { local opt="${1:-$(cat -)}"; print $((--opt)); }
+sum() { 
+  print "${@:-$(cat -)}" | 
+      awk '{for(i=1; i<=NF; i++) sum+=$i; } END {print sum}' 
+}
+## a number "object"
+@num() {
+  unsetopt warn_create_global
+  local name="${1}"
+  local value=${2}
+  declare -rg $name=$value
+  functions[$name]="print ${value}"  
+  eval "
+function $name { print ${value}; }
+alias -g $name="$name"
+"
+  function _n() {
+    val="$1"
+    function "$name".add() { local opt=$1; math.add "$val" "$opt" }
+    function "$name".sub() { local opt=$1; math.sub "$val" "$opt" }
+    function "$name".mul() { local opt=$1; math.mul "$val" "$opt" }
+    function "$name".div() { local opt=$1; math.div "$val" "$opt" }
+    function "$name".pow() { local opt=$1; math.pow "$val" "$opt" }
+    function "$name".mod() { local opt=$1; math.mod "$val" "$opt" }
+    function "$name".eq() { local opt=$1; math.eq "$val" "$opt" }
+    function "$name".ne() { local opt=$1; math.ne "$val" "$opt" }
+    function "$name".gt() { local opt=$1; math.gt "$val" "$opt" }
+    function "$name".lt() { local opt=$1; math.lt "$val" "$opt" }
+    function "$name".ge() { local opt=$1; math.ge "$val" "$opt" }
+    function "$name".le() { local opt=$1; math.le "$val" "$opt" }
+    function "$name".incr() { incr $val }
+    function "$name".decr() { decr $val }
+    function "$name".sum() { local args="$@"; sum "$args" }
+  }
+  _n "$value"
+}
+functions["@num"]="@num"  
+alias -g @num="@num"
+## ---------------------------------------------
+green "dsl/mathnum loaded"
