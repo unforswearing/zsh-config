d1ad4c32c0dee8157e5f447c8e0c9a26f539a193
unforswearing <al.charity@gmail.com>
2023-05-11 01:12:24 -0400

    create script to luad zsh functions from lua as files

--- bin/stdlib.zsh
+++ bin/stdlib.zsh
@@ -494,3 +494,40 @@ function sum() {
 }
 function calc() { print "$@" | bc; }
 ## ---------------------------------------------
+# create a standalone, top-level file for *almost* any zsh function
+# -> functions that use the ${1:-$(cat -)} construction wont work
+# for use with lua scripts via "luash"
+# ```lua
+# require("luash")
+# generate_binfile("incr")
+# print(incr(5))
+# ```
+# generated files are added to "/Users/unforswearing/zsh-config/src/bin"
+
+function delete_binfiles() {
+  /bin/rm -r /Users/unforswearing/zsh-config/src/bin/*
+  generate_binfile "generate_binfile"
+  generate_binfile "delete_binfiles"
+}
+function generate_binfile() {
+  local bindir="/Users/unforswearing/zsh-config/src/bin"
+  path+="$bindir"
+
+  local functionname="${1}"
+  local functionbody=$(get fn $functionname)
+
+  local binfile="${bindir}/${functionname}"
+  local argitems=("\\"" "$" "@" "\\"")
+
+  puts "#!/opt/local/bin/zsh" >| "$binfile"
+
+  {
+    puts "source \"${stdlib}\"";
+    puts "$functionbody";
+    puts "$functionname \"$(puts $argitems | sd " " "")\"";
+  } >>| "$binfile"
+
+  chmod +x "$binfile"
+}
+generate_binfile "generate_binfile"
+generate_binfile "delete_binfiles"
